bf252d104e6ea6aad3f145dc3493ea8283ae508f
Alejandro F <progressandro@gmail.com>
2020-03-07 18:41:35 -0400

    Another one.

---
+++ docs/precache-manifest.ce98f79e9c16f8b376272859ba90ad2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619ef7094d07da779f54952891f54178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ced6309fe22aeda5a7d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bdee3b4f9bad772914e5",
+    "url": "/static/js/2.f26264e1.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.f26264e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ced6309fe22aeda5a7d",
+    "url": "/static/js/main.94e01fd2.chunk.js"
+  },
+  {
+    "revision": "039f433ce0a2792034b4",
+    "url": "/static/js/runtime-main.0bb85c1a.js"
+  }
+]);
